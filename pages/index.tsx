0c3fafb8db5b3e0685055ccc09b9e9c5658a3071
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-19 22:28:46 -0300

    feat: :lipstick: generate view for all products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import {
+	Card,
+	CardActionArea,
+	CardMedia,
+	Grid,
+	Typography,
+} from '@mui/material';
 
 import { ShopLayout } from '@/components/layouts';
+import { initialData } from '../database/products';
 
 const HomePage: NextPage = () => {
 	return (
@@ -13,14 +20,38 @@ const HomePage: NextPage = () => {
 				variant='h1'
 				component='h1'
 			>
-				Start Proyect 🚀
+				Store
 			</Typography>
 			<Typography
 				variant='h2'
 				sx={{ mb: 1 }}
 			>
-				Ecommerce Next Js
+				All our products
 			</Typography>
+
+			<Grid
+				container
+				spacing={4}
+			>
+				{initialData.products.map(product => (
+					<Grid
+						item
+						xs={6}
+						sm={4}
+						key={product.slug}
+					>
+						<Card>
+							<CardActionArea>
+								<CardMedia
+									component='img'
+									image={`products/${product.images[0]}`}
+									alt={product.title}
+								/>
+							</CardActionArea>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
 		</ShopLayout>
 	);
 };
