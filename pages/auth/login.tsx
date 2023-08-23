2ec7d35de19e2a374662b7206c884835c97e6583
Martin Ferreira <martin.f.yic@gmail.com>
2023-08-22 21:53:24 -0300

    feat: :lipstick: add page/auth/login.tsx

---
+++ pages/auth/login.tsx
@@ -0,0 +1,87 @@
+import { NextPage } from 'next';
+import NextLink from 'next/link';
+import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+
+import { AuthLayout } from '@/components/layouts';
+
+const loginPage: NextPage = () => {
+	return (
+		<AuthLayout
+			title='Login'
+			pageDescription='User login page'
+		>
+			<Box sx={{ width: 350, padding: '10px 20px' }}>
+				<Grid
+					container
+					spacing={2}
+				>
+					<Grid
+						item
+						xs={12}
+					>
+						<Typography
+							variant='h1'
+							component='h1'
+							textAlign='center'
+						>
+							Authenticate
+						</Typography>
+					</Grid>
+
+					<Grid
+						item
+						xs={12}
+					>
+						<TextField
+							label='Email'
+							variant='filled'
+							fullWidth
+						/>
+					</Grid>
+
+					<Grid
+						item
+						xs={12}
+					>
+						<TextField
+							label='Password'
+							type='password'
+							variant='filled'
+							fullWidth
+						/>
+					</Grid>
+
+					<Grid
+						item
+						xs={12}
+					>
+						<Button
+							color='secondary'
+							className='circular-btn'
+							fullWidth
+						>
+							Login
+						</Button>
+					</Grid>
+
+					<Grid
+						item
+						xs={12}
+						display='flex'
+						justifyContent='end'
+					>
+						<Link
+							component={NextLink}
+							href='/auth/register'
+							passHref
+							underline='always'
+						>
+							Don&rsquo;t have an account?
+						</Link>
+					</Grid>
+				</Grid>
+			</Box>
+		</AuthLayout>
+	);
+};
+export default loginPage;
