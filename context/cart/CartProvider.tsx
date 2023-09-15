b0b9756580ee3d64e11c823099efcc3c9eb43062
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-14 21:14:19 -0300

    feat: :sparkles: add context to cart

---
+++ context/cart/CartProvider.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode, useReducer } from 'react';
+import { CartContext, cartReducer } from './';
+import { ICartProduct } from '../../interfaces/cart';
+
+export interface CartState {
+	cart: ICartProduct[];
+}
+
+const CART_INITIAL_STATE: CartState = {
+	cart: [],
+};
+
+interface Props {
+	children?: ReactNode;
+}
+
+export const CartProvider: FC<Props> = ({ children }) => {
+	const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
+
+	return (
+		<CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+	);
+};
