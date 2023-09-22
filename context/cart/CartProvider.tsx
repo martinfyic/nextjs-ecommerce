4560d6f3ab8fdf577e3f4ac8ae9174a0edcf4af0
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-22 17:38:55 -0300

    feat: :sparkles: at context/cart/CartProvider.tsx generate the logic to add product in cart "addProductToCart"

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -16,8 +16,38 @@ interface Props {
 
 export const CartProvider: FC<Props> = ({ children }) => {
 	const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
+	const addProductToCart = (product: ICartProduct) => {
+		const existProductInCart = state.cart.some(p => p._id === product._id);
+		if (!existProductInCart)
+			return dispatch({
+				type: '[Cart] - Update Cart',
+				payload: [...state.cart, product],
+			});
+
+		const productInCartDiffSize = state.cart.some(
+			p => p._id === product._id && p.size === product.size
+		);
+		if (!productInCartDiffSize)
+			return dispatch({
+				type: '[Cart] - Update Cart',
+				payload: [...state.cart, product],
+			});
+
+		const updatedProduct = state.cart.map(p => {
+			if (p._id !== product._id) return p;
+			if (p.size !== product.size) return p;
+
+			p.quantity += product.quantity;
+
+			return p;
+		});
+
+		dispatch({ type: '[Cart] - Update Cart', payload: updatedProduct });
+	};
 
 	return (
-		<CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+		<CartContext.Provider value={{ ...state, addProductToCart }}>
+			{children}
+		</CartContext.Provider>
 	);
 };
