6d8c1b3e14494c3649268a2fd15a6aded2d41263
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-22 18:54:45 -0300

    perf: :zap: implement cookies at cart

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,4 +1,6 @@
-import { FC, ReactNode, useReducer } from 'react';
+import { FC, ReactNode, useEffect, useReducer } from 'react';
+import Cookie from 'js-cookie';
+
 import { CartContext, cartReducer } from './';
 import { ICartProduct } from '../../interfaces/cart';
 
@@ -16,6 +18,28 @@ interface Props {
 
 export const CartProvider: FC<Props> = ({ children }) => {
 	const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
+
+	useEffect(() => {
+		try {
+			const getCookieProducts = Cookie.get('cart')
+				? JSON.parse(Cookie.get('cart')!)
+				: [];
+			dispatch({
+				type: '[Cart] - LoadCart from cookies | storage',
+				payload: getCookieProducts,
+			});
+		} catch (error) {
+			dispatch({
+				type: '[Cart] - LoadCart from cookies | storage',
+				payload: [],
+			});
+		}
+	}, []);
+
+	useEffect(() => {
+		if (state.cart.length > 0) Cookie.set('cart', JSON.stringify(state.cart));
+	}, [state.cart]);
+
 	const addProductToCart = (product: ICartProduct) => {
 		const existProductInCart = state.cart.some(p => p._id === product._id);
 		if (!existProductInCart)
