a608139fcb3b9b1c0b33253d7ba8ae9d8ccc3f17
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-22 18:35:57 -0300

    refactor: :sparkles: finish login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,10 +1,23 @@
+import { useState, useContext } from 'react';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import NextLink from 'next/link';
 
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import {
+	Box,
+	Button,
+	Chip,
+	Grid,
+	Link,
+	TextField,
+	Typography,
+} from '@mui/material';
+import { ErrorOutline } from '@mui/icons-material';
 import { useForm } from 'react-hook-form';
 
+import { AuthContext } from '@/context';
 import { AuthLayout } from '@/components/layouts';
+import { validations } from '@/utils';
 
 type FormData = {
 	email: string;
@@ -12,88 +25,139 @@ type FormData = {
 };
 
 const LoginPage: NextPage = () => {
+	const router = useRouter();
+	const { loginUser } = useContext(AuthContext);
+
 	const {
 		register,
 		handleSubmit,
-		watch,
 		formState: { errors },
 	} = useForm<FormData>();
+
+	const [showError, setShowError] = useState(false);
+
+	const onLoginUser = async ({ email, password }: FormData) => {
+		setShowError(false);
+
+		const isValidLogin = await loginUser(email, password);
+		if (!isValidLogin) {
+			setShowError(true);
+			setTimeout(() => {
+				setShowError(false);
+			}, 4000);
+			return;
+		}
+
+		router.replace('/');
+	};
+
 	return (
 		<AuthLayout
 			title='Login'
 			pageDescription='User login page'
 		>
-			<Box sx={{ width: 350, padding: '10px 20px' }}>
-				<Grid
-					container
-					spacing={2}
-				>
+			<form
+				onSubmit={handleSubmit(onLoginUser)}
+				noValidate
+			>
+				<Box sx={{ width: 350, padding: '10px 20px' }}>
 					<Grid
-						item
-						xs={12}
+						container
+						spacing={2}
 					>
-						<Typography
-							variant='h1'
-							component='h1'
-							textAlign='center'
+						<Grid
+							item
+							xs={12}
+							display='flex'
+							justifyContent='center'
+							flexDirection='column'
 						>
-							Authenticate
-						</Typography>
-					</Grid>
+							<Typography
+								variant='h1'
+								component='h1'
+								textAlign='center'
+							>
+								Authenticate
+							</Typography>
+							<Chip
+								label='Username / Password not valid'
+								color='error'
+								icon={<ErrorOutline />}
+								className='fadeIn'
+								sx={{ my: 1, display: showError ? 'flex' : 'none' }}
+							/>
+						</Grid>
 
-					<Grid
-						item
-						xs={12}
-					>
-						<TextField
-							label='Email'
-							variant='filled'
-							fullWidth
-						/>
-					</Grid>
-
-					<Grid
-						item
-						xs={12}
-					>
-						<TextField
-							label='Password'
-							type='password'
-							variant='filled'
-							fullWidth
-						/>
-					</Grid>
-
-					<Grid
-						item
-						xs={12}
-					>
-						<Button
-							color='secondary'
-							className='circular-btn'
-							fullWidth
+						<Grid
+							item
+							xs={12}
 						>
-							Login
-						</Button>
-					</Grid>
+							<TextField
+								fullWidth
+								label='Email'
+								type='email'
+								variant='filled'
+								{...register('email', {
+									required: 'This field is required',
+									validate: validations.isEmail,
+								})}
+								error={!!errors.email}
+								helperText={errors.email?.message}
+							/>
+						</Grid>
 
-					<Grid
-						item
-						xs={12}
-						display='flex'
-						justifyContent='end'
-					>
-						<Link
-							component={NextLink}
-							href='/auth/register'
-							passHref
-							underline='always'
+						<Grid
+							item
+							xs={12}
 						>
-							Don&rsquo;t have an account?
-						</Link>
+							<TextField
+								fullWidth
+								label='Password'
+								type='password'
+								variant='filled'
+								{...register('password', {
+									required: 'This field is required',
+									minLength: { value: 6, message: 'Min 6 characters' },
+								})}
+								error={!!errors.password}
+								helperText={errors.password?.message}
+							/>
+						</Grid>
+
+						<Grid
+							item
+							xs={12}
+						>
+							<Button
+								type='submit'
+								color='secondary'
+								className='circular-btn'
+								size='large'
+								fullWidth
+								disabled={showError}
+							>
+								Login
+							</Button>
+						</Grid>
+
+						<Grid
+							item
+							xs={12}
+							display='flex'
+							justifyContent='end'
+						>
+							<Link
+								component={NextLink}
+								href='/auth/register'
+								passHref
+								underline='always'
+							>
+								Don&rsquo;t have an account?
+							</Link>
+						</Grid>
 					</Grid>
-				</Grid>
-			</Box>
+				</Box>
+			</form>
 		</AuthLayout>
 	);
 };
