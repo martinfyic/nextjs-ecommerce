6fca1eeb8d90df2c1b0a4ede435014df3e853f1d
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-22 18:35:29 -0300

    feat: :sparkles: implement AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { SWRConfig } from 'swr';
 
-import { CartProvider, UiProvider } from '@/context';
+import { AuthProvider, CartProvider, UiProvider } from '@/context';
 import { lightTheme } from '@/themes';
 
 function App({ Component, pageProps }: AppProps) {
@@ -15,14 +15,16 @@ function App({ Component, pageProps }: AppProps) {
 					fetch(resource, init).then(res => res.json()),
 			}}
 		>
-			<CartProvider>
-				<UiProvider>
-					<ThemeProvider theme={lightTheme}>
-						<CssBaseline />
-						<Component {...pageProps} />
-					</ThemeProvider>
-				</UiProvider>
-			</CartProvider>
+			<AuthProvider>
+				<CartProvider>
+					<UiProvider>
+						<ThemeProvider theme={lightTheme}>
+							<CssBaseline />
+							<Component {...pageProps} />
+						</ThemeProvider>
+					</UiProvider>
+				</CartProvider>
+			</AuthProvider>
 		</SWRConfig>
 	);
 }
