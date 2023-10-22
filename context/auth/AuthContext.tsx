a06a2bcd2ff5e48326463a4b88d0efa50a950952
Martin Ferreira <martin.f.yic@gmail.com>
2023-10-22 18:34:32 -0300

    feat: :sparkles: Create Auth Context

---
+++ context/auth/AuthContext.tsx
@@ -0,0 +1,19 @@
+import { Context, createContext } from 'react';
+
+import { IUser } from '@/interfaces';
+import { IAuthRegister } from './';
+
+interface ContextProps {
+	isLoggedIn: boolean;
+	user?: IUser;
+	loginUser: (email: string, password: string) => Promise<boolean>;
+	registerUser: (
+		name: string,
+		email: string,
+		password: string
+	) => Promise<IAuthRegister>;
+}
+
+export const AuthContext: Context<ContextProps> = createContext(
+	{} as ContextProps
+);
