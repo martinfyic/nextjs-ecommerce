06a5feac8a4edb8f05f642acc1aba8421882c72e
Martin Ferreira <martin.f.yic@gmail.com>
2023-11-20 21:38:25 -0300

    refactor: :sparkles: create interfaces IOrder and IOrderItems

--- interfaces/order.ts
+++ interfaces/order.ts
@@ -1,3 +1,27 @@
+import { IShippingAddress, ISize, IUser } from './';
+
+export interface IOrder {
+	_id?: string;
+	user?: IUser | string;
+	orderItems: IOrderItems[];
+	shippingAddress: IShippingAddress;
+	paymentResult?: string;
+	order: IOrderSummary;
+	isPaid: boolean;
+	paidAt?: string;
+}
+
+export interface IOrderItems {
+	_id: string;
+	title: string;
+	size: ISize;
+	quantity: number;
+	slug: string;
+	image: string;
+	price: number;
+	gender: string;
+}
+
 export interface IOrderSummary {
 	numberOfItems: number;
 	subTotal: number;
